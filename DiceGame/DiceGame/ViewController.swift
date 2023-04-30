b9efe34933d29c7d5d6b70fb89d0f03eda841095
JSPark0099 <91583287+JSPark0099@users.noreply.github.com>
2023-04-30 16:06:59 +0900

    [Dice Game]start

---
+++ DiceGame/DiceGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceGame
+//
+//  Created by 김요한 on 2023/04/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
